df42c99149aaec69c4776cff9f1fc033b6f5be01
justin <justin.lee.gilpin@gmail.com>
2024-03-12 17:21:53 +0800

    update server

--- hosts/nixos/donchian/default.nix
+++ hosts/nixos/donchian/default.nix
@@ -1,8 +1,5 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ config, unstablePkgs, lib, pkgs, inputs, ... }:
 
 {
   imports =
@@ -15,23 +12,6 @@
 #      ./../../common/common-packages.nix
     ];
 
-#  nixpkgs = {
-#    config = {
-#      allowUnfree = true;
-#    };
-#  };
-
-#  nix = {
-#    settings = {
-      # Enable flakes and new 'nix' command
-#      experimental-features = "nix-command flakes";
-      # Deduplicate and optimize nix store
-#      auto-optimise-store = true;
-#      warn-dirty = false; # remove git warnings
-#    };
-#  };
-
-
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
@@ -40,7 +20,6 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
   
   # Tailscale Mesh VPN 
   services.tailscale.enable = true;
@@ -64,12 +43,6 @@
   };
 
   environment.systemPackages = with pkgs; [
-    neovim
-    wget
-    git
-    git-crypt
-    tailscale
-    cifs-utils
     lego # used for Let's Encrypt
     docker-compose
     docker
@@ -81,7 +54,6 @@
     jellyfin-web
     jellyfin-ffmpeg
     ffmpeg-headless
-    neofetch
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -102,28 +74,7 @@
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11"; 
 
 }
 
