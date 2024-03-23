aa0b0bb1ecdba7bfa69e56a501cbd23b885e539b
Justin <justin.lee.gilpin.com>
2024-03-24 07:23:06 +0800

    tried to nixify hyprlock

--- modules/wm/hyprland/hyprlock.nix
+++ modules/wm/hyprland/hyprlock.nix
@@ -1,104 +1,70 @@
-{inputs, ...}: {
-  imports = [
-    inputs.hyprlock.homeManagerModules.hyprlock
-  ];
-
+_: {
   programs.hyprlock = {
     enable = true;
-
-    general = {
-      disable_loading_bar = false;
-      hide_cursor = true;
-      grace = 2;
-      no_fade_in = false;
-    };
-
-    backgrounds = [
+    backgrounds = let
+      wallpaperPath = "/home/justin/.dots/home/images/wheatfield.png";
+    in [
       {
-        path = "";
-        color = "rgba(0, 0, 0, 0.5)";
-
-        # Blur
-        blur_passes = 3; # 0 disables blurring
-        blur_size = 10;
-        noise = 0.0117;
-        contrast = 0.8916;
-        brightness = 0.8172;
-        vibrancy = 0.1696;
-        vibrancy_darkness = 0.0;
+        monitor = "DP-1";
+        path = wallpaperPath;
+      }
+      {
+        monitor = "DP-2";
+        path = wallpaperPath;
+      }
+      {
+        monitor = "DP-3";
+        path = wallpaperPath;
+      }
+      {
+        monitor = "HDMI-A-1";
+        path = wallpaperPath;
       }
     ];
-
+    general = {
+      grace = 5;
+      disable_loading_bar = false;
+      hide_cursor = false;
+      no_fade_in = false;
+    };
     input-fields = [
       {
+        monitor = "DP-2";
         size = {
           width = 300;
           height = 50;
         };
-        outline_thickness = 3;
-        dots_size = 0.33;
-        dots_spacing = 0.15;
+        outline_thickness = 2;
+
+        outer_color = "rgb(f5c2e7)";
+        inner_color = "rgb(1e1e2e)";
+        font_color = "rgb(cdd6f4)";
+        placeholder_text = ''
+          <span foreground="##cdd6f4">Password...</span>
+        '';
+        fade_on_empty = false;
+        dots_spacing = 0.3;
         dots_center = true;
-        outer_color = "rgba(255, 255, 255, 0.1)";
-        inner_color = "rgba(255, 255, 255, 0.1)";
-        font_color = "rgb(255, 255, 255)";
-        fade_on_empty = true;
-        placeholder_text = "<i>Input Password...</i>";
-        hide_input = false;
-        position = {
-          x = 0;
-          y = -90;
-        };
-        halign = "center";
-        valign = "center";
       }
     ];
-
     labels = [
       {
-        text = "<b>$TIME</b>";
-        color = "rgb(255, 255, 255)";
-        font_size = 96;
-        position = {
-          x = 0;
-          y = 0;
-        };
-        halign = "center";
-        valign = "top";
-      }
-      {
-        text = ''cmd[update:1000] echo "$(date "+%a %d %b")"'';
-        color = "rgb(255, 255, 255)";
-        font_size = 24;
-        position = {
-          x = 0;
-          y = -150;
-        };
-        halign = "center";
-        valign = "top";
-      }
-      {
-        text = "Hi there, <i>$USER</i>!";
-        color = "rgb(255, 255, 255)";
-        font_size = 32;
-        position = {
-          x = 0;
-          y = 0;
-        };
-        halign = "center";
+        monitor = "DP-2";
+        text = "Hi, $USER";
+        color = "rgb(1e1e2e)";
         valign = "center";
+        halign = "center";
       }
       {
-        text = "";
-        color = "rgb(255, 255, 255)";
-        font_family = "monospace";
-        font_size = 32;
+        monitor = "DP-2";
+        text = "$TIME";
+        color = "rgb(1e1e2e)";
         position = {
           x = 0;
-          y = 0;
+          y = 120;
         };
+        valign = "center";
         halign = "center";
-        valign = "bottom";
       }
     ];
   };
