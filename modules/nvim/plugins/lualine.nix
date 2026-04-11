59e0425e8e47f7f986c5bea12d078b71614c3cc7
Justin Gilpin <justin.lee.gilpin@gmail.com>
2026-04-11 05:42:11 -0400

    update nvim

--- modules/nvim/plugins/lualine.nix
+++ modules/nvim/plugins/lualine.nix
@@ -1,60 +1,58 @@
 {
   programs.nixvim.plugins.lualine = {
     enable = true;
+    iconsEnabled = true;
 
     settings = {
       options = {
         globalstatus = true;
       };
-    # +-------------------------------------------------+
-    # | A | B | C                             X | Y | Z |
-    # +-------------------------------------------------+
-    sections = {
-      lualine_a = ["mode"];
-      lualine_b = ["branch"];
-      lualine_c = ["filename" "diff"];
 
-      lualine_x = [
-			  # hope to stop the diagonistics line from always appearing
-        "diagnostics"
-        # Show active language server
-        {
-          name.__raw = ''
-            function()
-              local msg = ""
-              local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-              local clients = vim.lsp.get_active_clients()
-              if next(clients) == nil then
+      sections = {
+        lualine_a = [ "mode" ];
+        lualine_b = [ "branch" ];
+        lualine_c = [ "filename" "diff" ];
+
+        lualine_x = [
+          "diagnostics"
+          {
+            name.__raw = ''
+              function()
+                local msg = ""
+                local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+                local clients = vim.lsp.get_active_clients()
+                if next(clients) == nil then
+                  return msg
+                end
+                for _, client in ipairs(clients) do
+                  local filetypes = client.config.filetypes
+                  if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                    return client.name
+                  end
+                end
                 return msg
               end
-              for _, client in ipairs(clients) do
-                local filetypes = client.config.filetypes
-                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                  return client.name
+            '';
+            icon = "";
+            color.fg = "#ffffff";
+          }
+          "encoding"
+          {
+            name.__raw = ''
+              function()
+                local status = require('ollama').status()
+                if status == "IDLE" then
+                  return "🦙"
+                elseif status == "WORKING" then
+                  return "⚙🦙"
                 end
               end
-              return msg
-            end
-          '';
-          icon = "";
-          color.fg = "#ffffff";
-        }
-        "encoding"
-        {
-          name.__raw = ''
-            function()
-              local status = require('ollama').status()
-              if status == "IDLE" then
-                return "🦙"
-              elseif status == "WORKING" then
-                return "⚙🦙"
-              end
-            end
-          '';
-          color.fg = "#ffffff";
-        }
-        "filetype"
-      ];
+            '';
+            color.fg = "#ffffff";
+          }
+          "filetype"
+        ];
+      };
     };
   };
 }
