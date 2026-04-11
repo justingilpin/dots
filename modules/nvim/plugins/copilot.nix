a752067c7860f6c1f25db33f9bca6f40f1ec4135
Justin Gilpin <justin.lee.gilpin@gmail.com>
2026-04-11 18:34:09 -0400

    update warnings

--- modules/nvim/plugins/copilot.nix
+++ modules/nvim/plugins/copilot.nix
@@ -1,46 +1,48 @@
 {
   programs.nixvim.plugins.copilot-lua = {
     enable = true;
-    panel = {
-      enabled = false; #false
-      autoRefresh = true;
-      keymap = {
-        jumpPrev = "[[";
-        jumpNext = "]]";
-        accept = "<CR>";
-        refresh = "gr";
-        open = "<leader>co"; # <M-CR>
+    settings = {
+      panel = {
+        enabled = false; #false
+        auto_refresh = true;
+        keymap = {
+          jump_prev = "[[";
+          jump_next = "]]";
+          accept = "<CR>";
+          refresh = "gr";
+          open = "<leader>co"; # <M-CR>
+        };
+        layout = {
+          position = "bottom"; # | top | left | right
+          ratio = 0.4;
+        };
       };
-      layout = {
-       position = "bottom"; # | top | left | right
-        ratio = 0.4;
+      suggestion = {
+        enabled = false; #false
+        auto_trigger = true;
+        debounce = 75;
+        keymap = {
+          accept = "<M-l>"; # <M-l>
+          accept_word = false;
+          accept_line = false;
+          next = "<M-]>";
+          prev = "<M-[>";
+          dismiss = "<C-]>";
+        };
       };
-    };
-    suggestion = {
-      enabled = false; #false
-      autoTrigger = true;
-      debounce = 75;
-      keymap = {
-        accept = "<M-l>"; # <M-l>	
-        acceptWord = false;
-        acceptLine = false;
-        next = "<M-]>";
-        prev = "<M-[>";
-        dismiss = "<C-]>";
+      filetypes = {
+        yaml = false;
+        markdown = false;
+        help = false;
+        gitcommit = false;
+        gitrebase = false;
+        hgcommit = false;
+        svn = false;
+        cvs = false;
+        "." = false;
       };
+      copilot_node_command = "node"; # Node.js version must be > 18.x
+      server_opts_overrides = {};
     };
-    filetypes = {
-      yaml = false;
-      markdown = false;
-      help = false;
-      gitcommit = false;
-      gitrebase = false;
-      hgcommit = false;
-      svn = false;
-      cvs = false;
-      "." = false;
-    };
-    copilotNodeCommand = "node"; # Node.js version must be > 18.x
-    serverOptsOverrides = {};
   };
 }
