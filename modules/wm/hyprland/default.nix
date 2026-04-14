85d8d5e50e9bd6ba2704b7f9c146224ff0cc9ac3
Justin <justin.lee.gilpin.com>
2026-04-14 22:43:21 +0800

    fix dolphin

--- modules/wm/hyprland/default.nix
+++ modules/wm/hyprland/default.nix
@@ -86,7 +86,7 @@
         env = [
           "XCURSOR_THEME,Adwaita"
           "XCURSOR_SIZE,24"
-          "QT_QPA_PLATFORMTHEME,qt5ct"
+          "QT_QPA_PLATFORMTHEME,kde"
         ];
 
         # ── Input ─────────────────────────────────────────────────────────────
